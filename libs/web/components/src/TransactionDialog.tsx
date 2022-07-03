0544910d905a17212db769688ff468e2668fe9f5
Ken Vu <ken.vu@centrality.ai>
2022-07-03 15:19:46 +1200

    Add `TransactionDialog`

---
+++ libs/web/components/src/TransactionDialog.tsx
@@ -0,0 +1,40 @@
+import { Dialog } from "@app-gov/web/components";
+import { ComponentProps, FC, useRef, useState, useCallback } from "react";
+
+interface TransactionDialogProps extends ComponentProps<typeof Dialog> {}
+
+export const TransactionDialog: FC<TransactionDialogProps> = ({
+	children,
+	...props
+}) => {
+	const focusElement = useRef(null);
+
+	return (
+		<Dialog
+			{...props}
+			initialFocus={focusElement}
+			panelClassName="w-1/2 max-w-2xl min-w-[480px]"
+		>
+			<div
+				ref={focusElement}
+				className="border-hero bg-light shadow-sharp shadow-dark flex  h-full flex-col items-center justify-center border-4 p-8"
+			>
+				{children}
+			</div>
+		</Dialog>
+	);
+};
+
+export const useTransactionDialog = () => {
+	const [open, setOpen] = useState<boolean>(false);
+
+	const openDialog = useCallback(() => {
+		setOpen(true);
+	}, []);
+
+	const closeDialog = useCallback(() => {
+		setOpen(false);
+	}, []);
+
+	return { open, openDialog, closeDialog };
+};
