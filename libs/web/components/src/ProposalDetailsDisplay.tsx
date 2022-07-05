599b5b2f44ed435ff3b3a351280e22aa84a73be3
aidan-starke <starke.aidan@gmail.com>
2022-07-06 11:14:28 +1200

    Update `Skeleton` component

--- libs/web/components/src/ProposalDetailsDisplay.tsx
+++ libs/web/components/src/ProposalDetailsDisplay.tsx
@@ -27,117 +27,106 @@ export const ProposalDetailsDisplay: FC<ProposalDetailsDisplayProps> = ({
 
 	return (
 		<div>
-			<div className="drop-shadow-sm space-y-4">
-				<Shadow gradientClassName="space-y-6">
-					<span className="text-4xl">
-						{proposalDetails ? (
-							<p>{proposalDetails?.title || "Untitled"}</p>
-						) : (
-							<Skeleton skeletonClassName="w-64 h-12" />
-						)}
-					</span>
+			<div className="drop-shadow-sm space-y-8">
+				<Shadow shadowClassName="text-lg space-y-4 group">
+					<Skeleton condition={!!proposalDetails} variant="large">
+						<div className="group-hover:border-l-4 group-hover:pl-2 duration-300 border-gray-400">
+							<Markdown input={proposalDetails?.description || "Undefined"} />
+						</div>
+					</Skeleton>
+				</Shadow>
+
+				<Shadow shadowClassName="space-y-6">
 					<div className="flex w-full space-x-20">
 						<div>
 							<span className="font-bold text-xl">Status</span>
-							{proposalStatus ? (
-								<p>{proposalStatus}</p>
-							) : (
-								<Skeleton skeletonClassName="w-32 h-6" />
-							)}
+							<Skeleton condition={!!proposalStatus} variant="small">
+								{proposalStatus}
+							</Skeleton>
 						</div>
 						<div>
 							<span className="font-bold text-xl">Enactment delay</span>
-							{proposalInfo ? (
-								<p>{proposalInfo?.enactmentDelay || 0} blocks</p>
-							) : (
-								<Skeleton skeletonClassName="w-32 h-6" />
-							)}
+							<Skeleton condition={!!proposalInfo} variant="small">
+								{proposalInfo?.enactmentDelay || 0} blocks
+							</Skeleton>
 						</div>
 					</div>
 					<div>
 						<span className="font-bold text-xl">Sponsor</span>
-						{proposalInfo ? (
-							<p>{proposalInfo.sponsor}</p>
-						) : (
-							<Skeleton skeletonClassName="w-2/3 h-6" />
-						)}
+						<Skeleton condition={!!proposalInfo} variant="long">
+							{proposalInfo?.sponsor}
+						</Skeleton>
 					</div>
-				</Shadow>
 
-				<If condition={section !== "undefined"}>
-					<Shadow>
-						<span className="font-bold text-xl">Proposed Call</span>
+					<If condition={section !== "undefined"}>
 						<div>
-							{section || method ? (
-								<p>
+							<span className="font-bold text-xl">Proposed Call</span>
+							<div>
+								<Skeleton condition={!!section || !!method} variant="small">
 									{section}.{method}
-								</p>
-							) : (
-								<Skeleton skeletonClassName="w-32 h-6" />
-							)}
-						</div>
-						<If condition={!!args}>
-							<div className="px-2 border border-hero rounded border-dotted shadow-sm mt-2">
-								<table className="w-full mt-2 mb-6">
-									<tbody>
-										<tr className="border-b border-dashed border-hero flex mb-2 py-2">
-											<th className="text-left w-1/2">Params</th>
-											<th className="text-right w-1/2">Values</th>
-										</tr>
-										{Object.keys(args || {})?.map((key) => (
-											<tr
-												key={key}
-												className="border-b border-hero flex py-2 items-center"
-											>
-												<td className="pr-20 flex-1">{key}</td>
-												<td className="break-all text-right">"{args[key]}"</td>
-											</tr>
-										))}
-									</tbody>
-								</table>
+								</Skeleton>
 							</div>
-						</If>
-					</Shadow>
-				</If>
-
-				<Shadow gradientClassName="text-lg space-y-4 group">
-					<span className="font-bold text-xl">Justification</span>
-					{proposalDetails ? (
-						<div className="group-hover:border-l-4 group-hover:pl-2 duration-300 border-gray-400">
-							<Markdown input={proposalDetails?.description || "Undefined"} />
+							<If condition={!!args}>
+								<div className="px-2 border border-hero rounded border-dotted shadow-sm mt-2">
+									<table className="w-full mt-2 mb-6">
+										<tbody>
+											<tr className="border-b border-dashed border-hero flex mb-2 py-2">
+												<th className="text-left w-1/2">Params</th>
+												<th className="text-right w-1/2">Values</th>
+											</tr>
+											{Object.keys(args || {})?.map((key) => (
+												<tr
+													key={key}
+													className="border-b border-hero flex py-2 items-center"
+												>
+													<td className="pr-20 flex-1">{key}</td>
+													<td className="break-all text-right">
+														"{args[key]}"
+													</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+								</div>
+							</If>
 						</div>
-					) : (
-						<div className="space-y-2">
-							<Skeleton skeletonClassName="w-32 h-6" />
-							<Skeleton skeletonClassName="w-56 h-6" />
-						</div>
-					)}
+					</If>
 				</Shadow>
 			</div>
 		</div>
 	);
 };
 
-interface SkeletonProps {
-	skeletonClassName: string;
+interface SkeletonProps extends PropsWithChildren {
+	condition: boolean;
+	variant: "small" | "large" | "long";
 }
 
-const Skeleton: FC<SkeletonProps> = ({ skeletonClassName }) => (
-	<div className="flex animate-pulse">
-		<div className={classNames(skeletonClassName, "bg-gray-600/50 rounded")} />
-	</div>
-);
+const Skeleton: FC<SkeletonProps> = ({ children, condition, variant }) => {
+	if (condition) return <div>{children}</div>;
+
+	return (
+		<div className="flex animate-pulse">
+			<div
+				className={classNames(
+					"bg-gray-600/50 rounded",
+					{ small: "w-32 h-4", large: "w-56 h-6", long: "w-2/3 h-4" }[variant]
+				)}
+			/>
+		</div>
+	);
+};
 
 interface ShadowProps extends PropsWithChildren {
-	gradientClassName?: string;
+	shadowClassName?: string;
 }
 
-const Shadow: FC<ShadowProps> = ({ gradientClassName, children }) => (
+const Shadow: FC<ShadowProps> = ({ shadowClassName, children }) => (
 	<div className="bg-hero py-2 pr-2 translate-x-2">
 		<div
 			className={classNames(
-				gradientClassName,
-				"p-4 flex flex-col justify-between h-full bg-mid border-4 border-dark -mt-4 -ml-2"
+				shadowClassName,
+				"p-4 bg-mid border-4 border-dark -mt-4 -ml-2"
 			)}
 		>
 			{children}
