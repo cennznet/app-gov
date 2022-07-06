9419facc8c434de3f70db55c4aef6fa2a952297c
Ken Vu <ken.vu@centrality.ai>
2022-07-06 16:18:41 +1200

    Apply new layout for New Proposal form

--- apps/gov-website/pages/proposal/new.tsx
+++ apps/gov-website/pages/proposal/new.tsx
@@ -1,32 +1,27 @@
-import type { Api, SubmittableResult } from "@cennznet/api";
-import type { NextPage } from "next";
-import { FormEventHandler, useCallback, useState } from "react";
+import type { GetStaticProps, NextPage } from "next";
 import { If } from "react-extras";
 
 import {
 	extractCallableExtrinsics,
 	getApiInstance,
 } from "@app-gov/service/cennznet";
-import { pinProposal } from "@app-gov/service/pinata";
+import { CENNZ_NETWORK } from "@app-gov/service/constants";
 import {
 	Button,
+	FunctionCallFieldSet,
 	Header,
 	Layout,
-	ProposalAdvanced,
-	ProposalDetailsField,
-	TextField,
+	MarkdownField,
+	Select,
 	WalletSelect,
 } from "@app-gov/web/components";
-import { useControlledInput } from "@app-gov/web/hooks";
-import { useCENNZApi, useCENNZWallet } from "@app-gov/web/providers";
-import type { ProposalCall } from "@app-gov/web/types";
-import { Spinner } from "@app-gov/web/vectors";
+import { useControlledCheckbox, useControlledInput } from "@app-gov/web/hooks";
 
 interface StaticProps {
 	extrinsics: Awaited<ReturnType<typeof extractCallableExtrinsics>>;
 }
 
-export const getStaticProps: GetStaticProps<StaticProps> = async (context) => {
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
 	const api = await getApiInstance(CENNZ_NETWORK.ChainSlug);
 	const extrinsics = await extractCallableExtrinsics(api);
 
@@ -38,11 +33,17 @@ export const getStaticProps: GetStaticProps<StaticProps> = async (context) => {
 };
 
 const NewProposal: NextPage<StaticProps> = ({ extrinsics }) => {
+	const { value: justification, onChange: onJustificationChange } =
+		useControlledInput<string, HTMLTextAreaElement>("");
+
+	const { value: functionCall, onChange: onFunctionCallChange } =
+		useControlledCheckbox(true);
+
 	return (
 		<Layout>
 			<Header />
 			<div className="w-full max-w-3xl flex-1 self-center px-8 pb-12">
-				<form onSubmit={onFormSubmit}>
+				<form>
 					<h1 className="font-display mb-8 text-center text-7xl uppercase">
 						Submit a Proposal
 					</h1>
@@ -55,82 +56,94 @@ const NewProposal: NextPage<StaticProps> = ({ extrinsics }) => {
 					</p>
 
 					<h2 className="font-display border-hero mb-4 border-b-2 text-4xl uppercase">
-						Connect your wallet
+						Proposal Details
 					</h2>
-					<p className="mb-8">
-						Lorem laborum dolor minim mollit eu reprehenderit culpa dolore
-						labore dolor mollit commodo do anim incididunt sunt id pariatur elit
-						tempor nostrud nulla eu proident ut id qui incididunt.
-					</p>
-					<fieldset className="mb-12 min-w-0">
-						<WalletSelect required />
+
+					<fieldset className="mb-6">
+						<label
+							className="mb-1 block text-lg font-bold"
+							htmlFor="justification"
+						>
+							Justification
+						</label>
+						<MarkdownField
+							required
+							id="justification"
+							value={justification}
+							onChange={onJustificationChange}
+						/>
 					</fieldset>
+
+					<fieldset className="mb-6">
+						<label
+							className="mb-1 block text-lg font-bold"
+							htmlFor="enacmentDelay"
+						>
+							Enactment Delay
+						</label>
+						<Select className="w-1/2" id="enacmentDelay">
+							<option value={24}>24 hours</option>
+							<option value={12}>12 hours</option>
+							<option value={6}>6 hours</option>
+							<option value={1}>1 hour</option>
+						</Select>
+					</fieldset>
+
+					<fieldset className="mb-6">
+						<label
+							htmlFor="functionCall"
+							className="flex cursor-pointer items-center"
+						>
+							<input
+								type="checkbox"
+								checked={functionCall}
+								onChange={onFunctionCallChange}
+								className="mr-1 inline-block"
+								id="functionCall"
+							/>
+							This proposal require a function call
+						</label>
+					</fieldset>
+
+					<If condition={functionCall}>
+						<h2 className="font-display border-hero mb-4 border-b-2 text-4xl uppercase">
+							Call Details
+						</h2>
+
+						<fieldset className="mb-6 grid grid-cols-2 gap-4">
+							<FunctionCallFieldSet.Provider extrinsics={extrinsics}>
+								<div className="cols-span-1">
+									<label className="mb-1 block text-lg font-bold">
+										Section
+									</label>
+									<FunctionCallFieldSet.Section />
+								</div>
+								<div className="cols-span-2">
+									<label className="mb-1 block text-lg font-bold">Method</label>
+									<FunctionCallFieldSet.Method />
+								</div>
+								<div className="col-span-full">
+									<label className="mb-1 block text-lg font-bold">
+										Arguments
+									</label>
+									<FunctionCallFieldSet.Args />
+								</div>
+							</FunctionCallFieldSet.Provider>
+						</fieldset>
+					</If>
 
 					<h2 className="font-display border-hero mb-4 border-b-2 text-4xl uppercase">
-						Enter proposal details
+						Signing Details
 					</h2>
-					<p className="mb-8">
-						The <em>justification</em> section supports markdown! Refer to{" "}
-						<a
-							href="https://assets.discohook.app/discord_md_cheatsheet.pdf"
-							target="_blank"
-							rel="noreferrer"
-							className="border-hero border-b italic"
-						>
-							this cheatsheet
-						</a>{" "}
-						to take advantage.
-					</p>
 
-					<fieldset className="space-y-6">
-						<div>
-							<label htmlFor="proposalTitle" className="text-lg">
-								Title
-							</label>
-							<TextField
-								id="proposalTitle"
-								name="proposalTitle"
-								inputClassName="w-full"
-								placeholder="|"
-								value={proposalTitle}
-								onChange={onProposalTitleChange}
-								required
-							/>
-						</div>
-
-						<ProposalDetailsField />
-
-						<div>
-							<label htmlFor="proposalDelay" className="text-lg">
-								Enactment Delay <span className="text-base">(# blocks)</span>
-							</label>
-							<TextField
-								id="proposalDelay"
-								name="proposalDelay"
-								type="number"
-								placeholder="1"
-								inputClassName="w-full"
-								value={proposalDelay}
-								onChange={onProposalDelayChange}
-								required
-							/>
-						</div>
+					<fieldset className="mb-6">
+						<WalletSelect required name="address" />
 					</fieldset>
 
-					<ProposalAdvanced
-						proposalCall={proposalCall}
-						updateProposalCall={updateProposalCall}
-					/>
-
 					<fieldset className="mt-16 text-center">
-						<Button type="submit" className="w-1/3 text-center" disabled={busy}>
+						<Button type="submit" className="w-1/3 text-center">
 							<div className="flex items-center justify-center">
-								<If condition={busy}>
-									<span className="mr-2">
-										<Spinner />
-									</span>
-								</If>
-								<span>{busy ? "Processing..." : "Sign and Submit"}</span>
+								<span>Sign and Submit</span>
 							</div>
 						</Button>
 						<p className="mt-2 text-sm">Estimated gas fee 2 CPAY</p>
@@ -142,75 +155,3 @@ const NewProposal: NextPage<StaticProps> = ({ extrinsics }) => {
 };
 
 export default NewProposal;
-
-const useProposal = () => {
-	const [proposalCall, setProposalCall] = useState<ProposalCall>();
-	const updateProposalCall = (section: string, value: string, arg?: string) =>
-		setProposalCall((prev) =>
-			section === "values"
-				? {
-						...prev,
-						[section]: { ...prev.values, [arg]: value },
-				  }
-				: {
-						...prev,
-						values: null,
-						[section]: value,
-				  }
-		);
-
-	return { proposalCall, updateProposalCall };
-};
-
-const getProposalExtrinsic = (
-	api: Api,
-	{ module: cennzModule, call, values }: ProposalCall
-) => api.tx[cennzModule][call](...Object.values(values));
-
-const useFormSubmit = (proposalCall: ProposalCall) => {
-	const [busy, setBusy] = useState<boolean>(false);
-
-	const { api } = useCENNZApi();
-	const { selectedAccount, wallet } = useCENNZWallet();
-	const signer = wallet?.signer;
-
-	const onFormSubmit: FormEventHandler<HTMLFormElement> = useCallback(
-		async (event) => {
-			event.preventDefault();
-
-			if (!api || !selectedAccount || !signer) return;
-			setBusy(true);
-
-			try {
-				const proposalData = new FormData(event.target as HTMLFormElement);
-
-				const { IpfsHash } = await pinProposal({
-					proposalTitle: proposalData.get("proposalTitle").toString(),
-					proposalDetails: proposalData.get("proposalDetails").toString(),
-				});
-
-				await api.tx.governance
-					.submitProposal(
-						proposalCall ? getProposalExtrinsic(api, proposalCall) : undefined,
-						PINATA_GATEWAY.concat(IpfsHash),
-						proposalData.get("proposalDelay")
-					)
-					.signAndSend(
-						selectedAccount.address,
-						{ signer },
-						(result: SubmittableResult) => {
-							const { txHash } = result;
-							console.info("Transaction", txHash.toString());
-						}
-					);
-			} catch (error) {
-				console.log("error", error.message);
-			}
-
-			setBusy(false);
-		},
-		[api, selectedAccount, signer, proposalCall]
-	);
-
-	return { busy, onFormSubmit };
-};
---
+++ libs/web/components/src/FunctionCallFieldSet.tsx
@@ -0,0 +1,167 @@
+import {
+	ChangeEventHandler,
+	ComponentProps,
+	createContext,
+	Dispatch,
+	FC,
+	SetStateAction,
+	useCallback,
+	useContext,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from "react";
+
+import {
+	extractCallableExtrinsics,
+	ExtrinsicArg,
+} from "@app-gov/service/cennznet";
+import { IntrinsicElements, PropsWithChildren } from "@app-gov/web/types";
+
+import { Select, TextField } from "./";
+
+type SelectProps = ComponentProps<typeof Select>;
+
+type CallableExtrinsics = Awaited<ReturnType<typeof extractCallableExtrinsics>>;
+
+interface ContextType {
+	sectionList?: string[];
+	methodList?: string[];
+	argList?: ExtrinsicArg[];
+	selectedSection?: string;
+	selectedMethod?: string;
+	setSelectedSection?: Dispatch<SetStateAction<string>>;
+	setSelectedMethod?: Dispatch<SetStateAction<string>>;
+}
+
+interface ProviderProps extends PropsWithChildren {
+	extrinsics: CallableExtrinsics;
+	defaultSection?: string;
+	defaultMethod?: string;
+}
+
+const Context = createContext({} as ContextType);
+
+const Provider: FC<ProviderProps> = ({
+	children,
+	extrinsics,
+	defaultSection = "system",
+	defaultMethod = "remark",
+}) => {
+	const [selectedSection, setSelectedSection] =
+		useState<string>(defaultSection);
+	const [selectedMethod, setSelectedMethod] = useState<string>(defaultMethod);
+	const sectionList = useMemo(() => Object.keys(extrinsics), [extrinsics]);
+	const methodList = useMemo(
+		() => Object.keys(extrinsics?.[selectedSection] ?? {}),
+		[extrinsics, selectedSection]
+	);
+	const firstRender = useRef<boolean>(true);
+	const argList = useMemo(
+		() => extrinsics?.[selectedSection]?.[selectedMethod],
+		[extrinsics, selectedMethod, selectedSection]
+	);
+
+	useEffect(() => {
+		if (firstRender.current) {
+			firstRender.current = false;
+			return;
+		}
+		setSelectedMethod(methodList?.[0]);
+	}, [methodList]);
+
+	return (
+		<Context.Provider
+			value={{
+				sectionList,
+				methodList,
+				argList,
+				selectedSection,
+				selectedMethod,
+				setSelectedSection,
+				setSelectedMethod,
+			}}
+		>
+			{children}
+		</Context.Provider>
+	);
+};
+
+const Section: FC<Omit<SelectProps, "children">> = (props) => {
+	const { selectedSection, setSelectedSection, sectionList } =
+		useContext(Context);
+	const onChange: ChangeEventHandler<HTMLSelectElement> = useCallback(
+		(event) => {
+			if (!setSelectedSection) return;
+			console.log(event.target.value);
+			setSelectedSection(event.target.value);
+		},
+		[setSelectedSection]
+	);
+
+	return (
+		<Select {...props} onChange={onChange} value={selectedSection}>
+			{sectionList?.map((section, index) => (
+				<option value={section} key={index}>
+					{section}
+				</option>
+			))}
+		</Select>
+	);
+};
+
+const Method: FC<Omit<SelectProps, "children">> = (props) => {
+	const { selectedMethod, setSelectedMethod, methodList } = useContext(Context);
+
+	const onChange: ChangeEventHandler<HTMLSelectElement> = useCallback(
+		(event) => {
+			if (!setSelectedMethod) return;
+			setSelectedMethod(event.target.value);
+		},
+		[setSelectedMethod]
+	);
+
+	return (
+		<Select {...props} onChange={onChange} value={selectedMethod}>
+			{methodList?.map((method, index) => (
+				<option value={method} key={index}>
+					{method}
+				</option>
+			))}
+		</Select>
+	);
+};
+
+const Args: FC<Omit<IntrinsicElements["ul"], "children">> = (props) => {
+	const { argList, selectedSection, selectedMethod } = useContext(Context);
+
+	return (
+		<ul {...props}>
+			{argList?.map((arg, index) => (
+				<li
+					className="mb-1 text-xs"
+					key={`${selectedSection}.${selectedMethod}-${index}`}
+				>
+					<TextField
+						placeholder={`\`${arg.type}\``}
+						className="font-mono"
+						required
+						startAdornment={
+							<span className="block w-24 border-r border-slate-200 font-mono">
+								{arg.name}
+							</span>
+						}
+					/>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export const FunctionCallFieldSet = {
+	Provider,
+	Section,
+	Method,
+	Args,
+};
