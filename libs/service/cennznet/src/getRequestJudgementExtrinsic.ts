91f28ab389ce7c2f78a0ec8df9ffa12038e1fddd
Ken Vu <ken.vu@centrality.ai>
2022-07-08 16:27:29 +1200

    Remove the need to specify second generic type `ISubmittableResult`

--- libs/service/cennznet/src/getRequestJudgementExtrinsic.ts
+++ libs/service/cennznet/src/getRequestJudgementExtrinsic.ts
@@ -1,10 +1,9 @@
 import { Api } from "@cennznet/api";
 import { SubmittableExtrinsic } from "@cennznet/api/types";
-import { ISubmittableResult } from "@cennznet/types";
 
 export const getRequestJudgementExtrinsic = (
 	api: Api,
 	registrarIndex: number
-): SubmittableExtrinsic<"promise", ISubmittableResult> => {
+): SubmittableExtrinsic<"promise"> => {
 	return api.tx.identity.requestJudgement(registrarIndex, 0);
 };
