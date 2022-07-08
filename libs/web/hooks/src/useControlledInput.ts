0b01b7471bd5a9a8eefc4b924926392ddd6c7545
Ken Vu <ken.vu@centrality.ai>
2022-07-08 17:18:36 +1200

    Add `resetValue` callback to `useControlledInput` hook

--- libs/web/hooks/src/useControlledInput.ts
+++ libs/web/hooks/src/useControlledInput.ts
@@ -1,8 +1,9 @@
-import { ChangeEvent, ChangeEventHandler, useState } from "react";
+import { ChangeEvent, ChangeEventHandler, useCallback, useState } from "react";
 
 interface ControlledInputHook<T, E> {
 	value: T;
 	onChange: ChangeEventHandler<E>;
+	resetValue: (value?: T) => void;
 }
 
 export const useControlledInput = <
@@ -16,20 +17,38 @@ export const useControlledInput = <
 	const onChange = (event: ChangeEvent<E>) =>
 		setValue(event.target.value as unknown as T);
 
+	const resetValue = useCallback(
+		(value = defaultValue) => {
+			setValue(value);
+		},
+		[defaultValue]
+	);
+
 	return {
 		value,
 		onChange,
+		resetValue,
 	};
 };
 
-export const useControlledCheckbox = (defaultValue: boolean) => {
+export const useControlledCheckbox = (
+	defaultValue: boolean
+): ControlledInputHook<boolean, HTMLInputElement> => {
 	const [value, setValue] = useState<boolean>(defaultValue);
 
 	const onChange = (event: ChangeEvent<HTMLInputElement>) =>
 		setValue(event.target.checked);
 
+	const resetValue = useCallback(
+		(value = defaultValue) => {
+			setValue(value);
+		},
+		[defaultValue]
+	);
+
 	return {
 		value,
 		onChange,
+		resetValue,
 	};
 };
