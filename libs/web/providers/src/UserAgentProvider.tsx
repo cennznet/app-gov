34195c24bab2780de04590ffff05a773ef067e48
aidan-starke <starke.aidan@gmail.com>
2022-07-13 13:03:05 +1200

    Revert "Move `UserAgentDialog` inside `UserAgentProvider`"
    This reverts commit 6edf1de6ec6696d97f84ce8adeff99f467ae6cf1.

--- libs/web/providers/src/UserAgentProvider.tsx
+++ libs/web/providers/src/UserAgentProvider.tsx
@@ -1,14 +1,6 @@
-import {
-	ComponentProps,
-	createContext,
-	FC,
-	useContext,
-	useEffect,
-	useState,
-} from "react";
+import { createContext, FC, useContext, useEffect, useState } from "react";
 import type { IBrowser, IDevice, IOS } from "ua-parser-js";
 
-import { Dialog } from "@app-gov/web/components";
 import { PropsWithChildren } from "@app-gov/web/types";
 
 type UserAgentContextType = {
@@ -32,7 +24,6 @@ export const UserAgentProvider: FC<UserAgentProviderProps> = ({
 	const [userAgent, setUserAgent] = useState<UserAgentContextType>(
 		{} as UserAgentContextType
 	);
-	const [open, setOpen] = useState<boolean>(false);
 
 	useEffect(() => {
 		import("ua-parser-js").then(({ default: UAParser }) => {
@@ -45,40 +36,13 @@ export const UserAgentProvider: FC<UserAgentProviderProps> = ({
 		});
 	}, [value]);
 
-	useEffect(() => {
-		if (!userAgent?.browser?.name) return;
-
-		if (
-			userAgent?.browser.name === "Firefox" ||
-			userAgent?.browser.name === "Safari"
-		)
-			setOpen(true);
-	}, [userAgent?.browser?.name]);
-
 	return (
-		<div>
-			<UserAgentContext.Provider value={userAgent}>
-				{children}
-			</UserAgentContext.Provider>
-
-			<BrowsersDialog open={open} onClose={() => setOpen(false)} />
-		</div>
+		<UserAgentContext.Provider value={userAgent}>
+			{children}
+		</UserAgentContext.Provider>
 	);
 };
 
 export function useUserAgent(): UserAgentContextType {
 	return useContext(UserAgentContext);
 }
-
-export interface BrowsersDialogProps extends ComponentProps<typeof Dialog> {}
-
-const BrowsersDialog: FC<BrowsersDialogProps> = (props) => (
-	<Dialog {...props}>
-		<div className="border-hero bg-light shadow-sharp shadow-dark -mx-[4em] flex h-full flex-col items-center justify-center border-4 p-8">
-			<p className="prose w-full text-center text-base">
-				Sorry, this browser is not supported by this app. To participate in our
-				Governance Platform, please use Chrome or its variants on a Mac or PC.
-			</p>
-		</div>
-	</Dialog>
-);
