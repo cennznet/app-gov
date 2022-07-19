3efb45290053e6517d12201a6e0ecdaea8c6f6cb
aidan-starke <starke.aidan@gmail.com>
2022-07-19 14:59:53 +1200

    Port over `getDiscordWebhook` from `gov-services`

--- libs/service/discord/src/getDiscordBot.ts
+++ libs/service/discord/src/getDiscordBot.ts
@@ -1,4 +1,5 @@
 import { Client as DiscordClient, Intents } from "discord.js";
+import type { InteractionWebhook } from "discord.js";
 
 const bot = new DiscordClient({
 	partials: ["CHANNEL"],
@@ -16,6 +17,32 @@ export const getDiscordBot = (token: string): Promise<DiscordClient> =>
 			bot.login(token);
 
 			bot.on("ready", () => resolve(bot));
+		} catch (error) {
+			reject(error);
+		}
+	});
+
+export const getDiscordWebhook = (
+	token: string,
+	channelId: string,
+	webhookId: string
+): Promise<InteractionWebhook> =>
+	new Promise((resolve, reject) => {
+		try {
+			bot.login(token);
+
+			bot.on("ready", async () => {
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				const channel: any = bot.channels.cache.get(channelId);
+
+				const webhook = await channel
+					.fetchWebhooks()
+					.then((hooks: InteractionWebhook[]) =>
+						hooks.find((hook) => hook.id === webhookId)
+					);
+
+				resolve(webhook);
+			});
 		} catch (error) {
 			reject(error);
 		}
