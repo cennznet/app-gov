46dcdfedf05030254067fcccacd5825224dd987b
Ken Vu <ken.vu@centrality.ai>
2022-07-20 17:06:47 +1200

    Make `resolveProposalJustification` safer with white-listing domains

--- apps/gov-website/pages/proposal/[pid].tsx
+++ apps/gov-website/pages/proposal/[pid].tsx
@@ -33,7 +33,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 		proposal.justificationUri
 	);
 
-	if (!proposal)
+	if (!proposal || !justification)
 		return {
 			notFound: true,
 		};
