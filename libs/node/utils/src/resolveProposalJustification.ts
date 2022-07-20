aa66e94e224e9c95067c6de517a76ff9240b98ae
aidan-starke <starke.aidan@gmail.com>
2022-07-20 14:03:09 +1200

    Avoid build fail from bad `justificationUri`

--- libs/node/utils/src/resolveProposalJustification.ts
+++ libs/node/utils/src/resolveProposalJustification.ts
@@ -10,10 +10,19 @@ import { safeFetch } from "./";
  */
 export const resolveProposalJustification = async (
 	justificationUri: string
-): Promise<string> => {
+): Promise<string | void> => {
 	const fallback = `Justification details is published [here](${justificationUri})`;
 
 	if (justificationUri.indexOf(PINATA_GATEWAY) < 0) return fallback;
-	const response = await safeFetch(justificationUri);
+	let response;
+
+	try {
+		response = await safeFetch(justificationUri);
+	} catch (error) {
+		console.error(error);
+	}
+
+	if (!response) return;
+
 	return (await response.json())?.justification ?? fallback;
 };
