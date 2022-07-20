efc5308811b328678c504c860c19945a2e75e5e1
aidan-starke <starke.aidan@gmail.com>
2022-07-20 14:15:57 +1200

    Refactor sidebar colours, only display `proposalDetails` if they can be fetched successfully

--- libs/service/discord/src/getProposalFields.ts
+++ libs/service/discord/src/getProposalFields.ts
@@ -1,4 +1,4 @@
-import type { EmbedFieldData } from "discord.js";
+import type { ColorResolvable, EmbedFieldData } from "discord.js";
 import { MessageEmbed } from "discord.js";
 
 import { resolveProposalJustification } from "@app-gov/node/utils";
@@ -6,40 +6,66 @@ import type { ProposalModel } from "@app-gov/service/mongodb";
 
 import { getVoteFields } from "./";
 
+const COLOURS: Record<string, ColorResolvable> = {
+	Pass: "#05b210",
+	Vote: "#9847FF",
+	Reject: "RED",
+};
+
 export const getProposalEmbed = async (
 	proposalId: number,
 	proposalInfo: ProposalModel
-): Promise<MessageEmbed> =>
-	proposalInfo.status?.includes("Deliberation")
+): Promise<MessageEmbed> => {
+	const status = proposalInfo.status;
+	const proposalFields = await getProposalFields(proposalInfo);
+
+	return proposalInfo.status?.includes("Deliberation")
 		? new MessageEmbed()
-				.setColor("#9847FF")
+				.setColor(COLOURS.Vote)
 				.setTitle(`Proposal ID: _#${proposalId}_`)
-				.setFields(await getProposalFields(proposalInfo))
+				.setFields(proposalFields)
 				.addFields(getVoteFields(proposalInfo))
-				.setFooter({ text: `Status: ${proposalInfo.status}` })
+				.setFooter({ text: `Status: ${status}` })
 				.setTimestamp()
 		: new MessageEmbed()
-				.setColor(proposalInfo.status === "Disapproved" ? "RED" : "#05b210")
+				.setColor(status === "Disapproved" ? COLOURS.Reject : COLOURS.Pass)
 				.setTitle(`Proposal ID: _#${proposalId}_`)
-				.setFields(await getProposalFields(proposalInfo))
-				.setFooter({ text: `Status: ${proposalInfo.status}` })
+				.setFields(proposalFields)
+				.setFooter({ text: `Status: ${status}` })
 				.setTimestamp();
+};
 
 export const getProposalFields = async (
 	proposalInfo: ProposalModel
-): Promise<EmbedFieldData[]> => [
-	{
-		name: "Details",
-		value: await resolveProposalJustification(
-			proposalInfo.justificationUri ?? ""
-		),
-	},
-	{
-		name: "Sponsor",
-		value: `_${proposalInfo.sponsor}_`,
-	},
-	{
-		name: "Enactment Delay",
-		value: `${proposalInfo.enactmentDelay} blocks`,
-	},
-];
+): Promise<EmbedFieldData[]> => {
+	const proposalDetails = await resolveProposalJustification(
+		proposalInfo.justificationUri ?? ""
+	);
+
+	if (proposalDetails)
+		return [
+			{
+				name: "Details",
+				value: proposalDetails ?? "",
+			},
+			{
+				name: "Sponsor",
+				value: `_${proposalInfo.sponsor}_`,
+			},
+			{
+				name: "Enactment Delay",
+				value: `${proposalInfo.enactmentDelay} blocks`,
+			},
+		];
+
+	return [
+		{
+			name: "Sponsor",
+			value: `_${proposalInfo.sponsor}_`,
+		},
+		{
+			name: "Enactment Delay",
+			value: `${proposalInfo.enactmentDelay} blocks`,
+		},
+	];
+};
