b615aadd8470b7f753e5d0e3179f7f77d9353666
aidan-starke <starke.aidan@gmail.com>
2022-07-25 15:56:14 +1200

    Return early if no `justificationUri`

--- apps/gov-website/pages/proposal/[pid].tsx
+++ apps/gov-website/pages/proposal/[pid].tsx
@@ -57,8 +57,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
 		.findOne({ proposalId: pid })
 		.lean();
 
+	if (!proposal?.justificationUri)
+		return {
+			notFound: true,
+		};
+
 	const justification = await resolveProposalJustification(
-		proposal?.justificationUri ?? ""
+		proposal.justificationUri
 	);
 
 	const vetoThreshold = await fetchProposalVetoThreshold(api);
