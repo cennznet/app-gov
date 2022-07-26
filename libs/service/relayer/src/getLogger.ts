26117a5ead9be8cbad414caf28d2e4e8915d3145
Ken Vu <97480229+ken-centrality@users.noreply.github.com>
2022-07-27 10:34:53 +1200

    Add a `proposal-patch <start> <end>` command to patch up DB (#91)

--- libs/service/relayer/src/getLogger.ts
+++ libs/service/relayer/src/getLogger.ts
@@ -1,7 +1,7 @@
 import chalk from "chalk";
 import { createLogger, format, Logger, transports } from "winston";
 
-type LoggerService = "ProposalPub" | "ProposalSub";
+type LoggerService = "ProposalPub" | "ProposalSub" | "ProposalPatch";
 
 const instances = {} as Record<LoggerService, Logger>;
 
