12eb4dce8e5e8f239fe028cd2b47cd23274e74e0
aidan-starke <starke.aidan@gmail.com>
2022-11-14 12:12:37 +1300

    Debug log `lastKnownId`, `endId`

--- libs/service/relayer/src/monitorNewProposal.ts
+++ libs/service/relayer/src/monitorNewProposal.ts
@@ -1,11 +1,13 @@
 import { Api } from "@cennznet/api";
 import { Mongoose } from "mongoose";
+import { Logger } from "winston";
 
 import { ProposalModel } from "@app-gov/service/mongodb";
 
 export const monitorNewProposal = async (
 	api: Api,
 	mdb: Mongoose,
+	logger: Logger,
 	callback: (proposalId: number) => void
 ): Promise<void> => {
 	const nextProposalId = await api.query.governance.nextProposalId();
@@ -16,6 +18,7 @@ export const monitorNewProposal = async (
 				?.proposalId ?? -1
 		) + 1;
 	const endId = nextProposalId.toJSON() as number;
+	logger.info(JSON.stringify({ lastKnownId, endId }));
 	const proposalIds = [];
 	for (let i = lastKnownId; i < endId; i++) proposalIds.push(i);
 	proposalIds.forEach(callback);
